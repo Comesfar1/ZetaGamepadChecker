c19541c6b62aa89a535dc1a3fcf55ba20182fe5c
Comesfar1 <durduemirberke11@gmail.com>
2026-04-15 22:34:32 +0300

    Create App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+
+namespace ZetaCheckCore
+{
+    public partial class App : Application
+    {
+        // Uygulamanın başlatıcı sınıfı (Arka planda Main metodunu otomatik üretir)
+    }
+}
